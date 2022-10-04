18268583a3c2b774ec795b2c0f4c3ca6dc8abc2f
singhanupam6 <anupamrajawat4@gmail.com>
2022-10-04 17:49:50 +0530

    operators

---
+++ index.js
@@ -0,0 +1,42 @@
+let a = 10;
+let b = 20;
+console.log("Initialized Values a=",a,"b=",b);
+console.log("Arithimatic operations:");
+console.log("a+b: ",a+b,"a-b:", a-b,"a*b:", a*b,"a/b:", a/b,"a%b:", a%b);
+
+
+
+console.log("Relational operations: ");
+console.log("a>b, a<b, a<=b, a>=b  :",a>b, a<b, a<=b, a>=b);
+
+
+
+console.log("Increment/decrement Operations: ");
+console.log("a++,b++,a--,b--  :",a++,b++,a--,b--);
+
+
+
+console.log("Bitwise Operations: ");
+console.log("a & b, a|b, a^b, ~a, ~b, a<<b, a>>b, a>>>b   :",a & b, a|b, a^b, ~a, ~b, a<<b, a>>b, a>>>b);
+
+
+
+console.log("Assignment Operations: ");
+let c=a;
+let d=b;
+console.log("c= ",c,"d= ",d);
+
+
+
+console.log("Conditional Operations: ")
+console.log("a>b?a is greater':b is greater   :",a>b?"a is greater":"b is greater");
+
+
+
+console.log("Special Operatons: ");
+console.log("Type of a is :",typeof(a));
+
+
+
+console.log("Logical Operations: ");
+console.log("a && b, a || b, !a   :",a && b, a || b, !a);
